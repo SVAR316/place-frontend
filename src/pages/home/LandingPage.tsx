03482399aadf69319a19966b0c2f54269cba6fa3
Artem <dt2csgo@mail.ru>
2024-11-26 00:10:50 +0400

    update

--- src/pages/home/LandingPage.tsx
+++ src/pages/home/LandingPage.tsx
@@ -2,6 +2,9 @@ import HeaderLeading from "../../widgets/header/common/headerLeading";
 import './style.scss'
 
 
+import previewImage from '../../shared/images/landing/preview.png'
+import usersLan from '../../shared/images/landing/users-lan.svg'
+
 function LandingPage() {
 
 
@@ -11,21 +14,32 @@ function LandingPage() {
             <div className={'container'}>
                 <div className={'preview'}>
                     <div className={'preview-left'}>
-                        <p className={'preview-left__title'}>
+                        <h1 className={'preview-left__title'}>
                             Платформа для добрых дел
-                        </p>
-                        <p className={'preview-left__desc'}>
+                        </h1>
+                        <h2 className={'preview-left__desc'}>
                             Добро пожаловать на "Добрые Дела" – уникальную платформу, созданную для тех, кто стремится
                             изменить мир к лучшему! Мы объединяем людей, готовых совершать добрые дела, и тех, кто
                             нуждается в помощи. Наша миссия – сделать волонтерство доступным, понятным и вдохновляющим
                             для каждого.
-                        </p>
+                        </h2>
                         <div>
 
                         </div>
                     </div>
                     <div className={'preview-right'}>
+                        <img className={'preview-right__image'} src={previewImage} alt={'preview image'} />
 
+                        <div>
+                            <div>
+                                <div>
+                                    <img src={usersLan} alt={'users icon'} />
+                                    <h3>
+                                        Сообщество единомышленников
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
