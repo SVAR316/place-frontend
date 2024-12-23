af1184067034ef96814eaf0ae548f64b1df2b34b
Artem <dt2csgo@mail.ru>
2024-12-23 23:39:08 +0400

    feat: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import {lazy, Suspense} from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 
 const LandingPage = lazy(() => import('./home/LandingPage.tsx'))
+const NewsPage = lazy(() => import('./news/page/News.tsx'))
 
 
 export const Routing: React.FC = () => {
@@ -13,6 +14,8 @@ export const Routing: React.FC = () => {
             <Router>
                 <Routes>
                     <Route path={'/'} element={<LandingPage />} />
+                    <Route path={'/news/:id'} element={<NewsPage />} />
+                    <Route path={'*'} element={<Navigate to={'/'} />} />
                 </Routes>
             </Router>
         </Suspense>
